304d5db8e23324f76bf7b934156c29272c3a91d3
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-04 18:21:31 +0200

    #6/#22/feat: create a basic layout for the page with a mix of grid and flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
+import MaxWidthWrapper from "@/components/layout/max-width-wrapper";
 import { Logotype } from "@/components/ui/logotype";
 import { Check } from "lucide-react";
-import Image from "next/image";
 
 export default async function Home() {
   return (
@@ -20,25 +20,47 @@ export default async function Home() {
       </section>
 
       <main>
-        {/* About us -- cta-overview */}
-        {/* TODO: update Lorem ipsum text content */}
-        <section className="grid grid-cols-2 py-10">
-          <h2 className="flex justify-center items-center">Why Memovies?</h2>
-          <ul className="flex flex-col gap-5">
-            <li className="flex items-center gap-3">
-              <Check></Check>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            </li>
-            <li className="flex items-center gap-3">
-              <Check></Check>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            </li>
-            <li className="flex items-center gap-3">
-              <Check></Check>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            </li>
-          </ul>
-        </section>
+        <MaxWidthWrapper className="grid grid-cols-5">
+          {/* About us -- cta-overview */}
+          {/* TODO: update Lorem ipsum text content */}
+          <section className="py-10 grid grid-cols-2 col-span-5">
+            <h2 className="flex items-center font-secondary font-semibold">
+              Why Memovies?
+            </h2>
+            <ul className="flex flex-col gap-5">
+              <li className="flex items-center gap-3">
+                <Check></Check>
+                <p className="text-xs">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </li>
+              <li className="flex items-center gap-3">
+                <Check></Check>
+                <p className="text-xs">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </li>
+              <li className="flex items-center gap-3">
+                <Check></Check>
+                <p className="text-xs">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </li>
+            </ul>
+          </section>
+
+          {/* Selected movies */}
+          <h2 className="text-center font-secondary font-semibold col-span-5">
+            Selected movies
+          </h2>
+          {/* TODO: import selected-movies component here */}
+
+          {/* Movie genre / Categories */}
+          <h2 className="text-center font-secondary font-semibold col-span-5">
+            Categories
+          </h2>
+          {/* TODO: import categories component here */}
+        </MaxWidthWrapper>
       </main>
     </>
   );
