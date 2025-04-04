6456b0cbc9c13ded34a80e757a8299a18a1ae0b4
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-04 12:26:11 +0200

    #6/#22/feat: create hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import { Logotype } from "@/components/ui/logotype";
+import Image from "next/image";
+
 export default async function Home() {
   return (
     <>
-      <h1>HOMEPAGE</h1>
+      {/* Herosection */}
+      <section className="relative bg-[url('/assets/images/popcorn.jpg')] bg-cover bg-center w-full h-screen flex justify-center items-center text-background">
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-foreground opacity-30"></div>
+
+        {/* Overlay content */}
+        <div className="absolute flex flex-col items-center justify-center text-background">
+          <h1 className="font-secondary text-2xl font-semibold">
+            Timeless movies, endless memories
+          </h1>
+          <Logotype></Logotype>
+        </div>
+      </section>
     </>
   );
 }
