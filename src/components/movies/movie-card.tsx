6fcd3451568870027956ce38b5cb5f5872b1ca6b
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-08 15:34:55 +0200

    #12/#27/feat: new component movie-card to display the movies in movies-list.

---
+++ src/components/movies/movie-card.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+import {
+  Card,
+  CardContent,
+  // CardFooter, //Save for later
+  // CardHeader, //Save for later
+  CardTitle,
+} from "@/components/ui/card";
+import { Movie } from "@/lib/movies/interfaces";
+
+export const MovieCard = ({ movie }: { movie: Movie }) => {
+  return (
+    <Card className="overflow-hidden rounded-lg">
+      <CardTitle className="sr-only">{movie.title}</CardTitle>
+      <CardContent className="relative h-[300px] p-0">
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={`Movie Poster of ${movie.title}`}
+          fill
+          className="object-cover"
+        />
+      </CardContent>
+    </Card>
+  );
+};
