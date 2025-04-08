362e590f04fd5f91520ea62da858369ebe633578
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-08 10:02:36 +0200

    #12/#38/feat: function to fetch movies by year, and add isFavourite and onWatchList as boolean properties

--- src/lib/movies/action.ts
+++ src/lib/movies/action.ts
@@ -1,7 +1,8 @@
 "use server";
 
-const API_ENDPOINT =
-  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+import { Movie, MoviesApiData } from "./interfaces";
+
+const API_ENDPOINT = "https://api.themoviedb.org/3";
 
 const TOKEN = process.env.ACCESS_TOKEN_AUTH;
 
@@ -14,7 +15,10 @@ const options = {
 };
 
 export async function fetchNowPlayingMovies() {
-  const res = await fetch(API_ENDPOINT, options);
+  const res = await fetch(
+    API_ENDPOINT + "/movie/now_playing?language=en-US&page=1",
+    options
+  );
 
   if (!res.ok) {
     throw new Error(`Error HTTP status: ${res.status}`);
@@ -22,4 +26,43 @@ export async function fetchNowPlayingMovies() {
 
   const data = await res.json();
   return data;
+}
+
+export async function fetchMoviesByYear(query: number): Promise<Movie[]> {
+  try {
+    // Fetch data
+    const res = await fetch(
+      `${API_ENDPOINT}/discover/movie?include_adult=false&include_video=false&language=en-US&primary_release_date.gte=${query}-01-01&primary_release_date.lte=${query}-12-31&region=se&sort_by=popularity.desc&with_release_type=2|3&with_original_language=en|sv`,
+      options
+    );
+
+    // Check response
+    if (!res.ok) {
+      throw new Error(`Error HTTP status: ${res.status}`);
+    }
+
+    const movieData: MoviesApiData = await res.json();
+
+    // Check data format
+    if (!Array.isArray(movieData.results)) {
+      throw new Error("Invalid data format received");
+    }
+
+    // Add new properties to Movie object
+    const updatedMovies: Movie[] = movieData.results.map((movie) => ({
+      ...movie,
+
+      isFavourite: Boolean(false),
+      onWatchList: Boolean(false),
+    }));
+
+    console.log(
+      "------------- SVAR DITT FILMÅR *** UPPDATERAD DATA ----------------",
+      updatedMovies
+    );
+    return updatedMovies;
+  } catch (error) {
+    console.error("Error, can not fetch data:", error);
+    throw error;
+  }
 }
