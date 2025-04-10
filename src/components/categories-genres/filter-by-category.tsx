d5453da6a15cdcb7f7b25e095f85dd6e655ba1ee
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-10 19:51:18 +0200

    #15/feat: create functionality to render movies by selected category

--- src/components/categories-genres/filter-by-category.tsx
+++ src/components/categories-genres/filter-by-category.tsx
@@ -14,8 +14,32 @@ import {
 import { fetchGenres } from "@/lib/movies/action";
 import { Genre } from "@/lib/categories-genres/interfaces";
 
-export const FilterByCategory = () => {
+interface FilterByCategoryProps {
+  setSelectedCategory: (category: Genre | null) => void;
+}
+
+export const FilterByCategory = ({
+  setSelectedCategory,
+}: FilterByCategoryProps) => {
+  // State for categories to be able to save categories and save that list in LS
   const [categories, setCategories] = useState<Genre[]>([]);
+
+  //Function to handle category change - Select
+  const handleCategoryChange = (value: string) => {
+    if (value === "all") {
+      setSelectedCategory(null); // Reset
+    } else {
+      // Find category
+      const selectedCategoryObj = categories.find(
+        (movie) => movie.name === value
+      );
+
+      // If selected category = set the selectedCategory state
+      if (selectedCategoryObj) {
+        setSelectedCategory(selectedCategoryObj);
+      }
+    }
+  };
 
   useEffect(() => {
     // Key in local storage (LS)
@@ -54,13 +78,14 @@ export const FilterByCategory = () => {
   }, []);
 
   return (
-    <Select>
+    <Select onValueChange={handleCategoryChange}>
       <SelectTrigger className="w-[180px] my-10">
         <SelectValue placeholder="Filter by category" />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           <SelectLabel>Categories</SelectLabel>
+          <SelectItem value="all">All Categories</SelectItem>
           {categories.map((category) => (
             <SelectItem key={category.id} value={category.name}>
               {category.name}
