1737efc98ec66a38f92a37d31d9bb449ae63e2c8
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-10 11:11:29 +0200

    #6/#29/feat: responsive grid-layout to all movies-list on the HomePage

--- src/components/movies/top-rated-movies.tsx
+++ src/components/movies/top-rated-movies.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { MovieCard } from "./movie-card";
 import { Movie } from "@/lib/movies/interfaces";
 import { Rating } from "../ui/rating";
+import clsx from "clsx";
 
 export const TopRatedMovies = async () => {
   // Fetch toprated movies
@@ -14,9 +15,19 @@ export const TopRatedMovies = async () => {
   return (
     <>
       {topRatedMovies ? (
-        <ul className="grid justify-center grid-cols-2 gap-3 px-3 md:grid-cols-5 md:gap-8">
-          {topRatedMovies.map((movie) => (
-            <li key={movie.id}>
+        <ul className="grid justify-center grid-cols-2 gap-3 px-3 sm:grid-cols-4 md:grid-cols-5 md:gap-y-8">
+          {topRatedMovies.map((movie, index) => (
+            <li
+              key={movie.id}
+              className={clsx(
+                // for sm devices - the first two movie-card span 2 cols
+                index < 2 ? "sm:col-span-2" : "sm:col-span-1",
+                // from md all movie-card span 1 col
+                "md:col-span-1",
+                // Baselayout
+                "col-span-1"
+              )}
+            >
               <MovieCard movie={movie} />
               {/* Slice the year from date */}
               <h3 className="my-3 text-lg">
