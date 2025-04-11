a2c0c28d1ac1558273ca7775e30eb19d4e41bcd5
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-11 16:49:39 +0200

    #68/fix: need a new category select. Delete the old one since it only cause errors. The new one will be built later.

---
+++ src/components/navigation/category-select.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { use, useCallback } from "react";
+import { Genre } from "@/lib/interfaces/category-interfaces";
+
+export const CategorySelect = ({
+  categories,
+}: {
+  categories: Promise<Genre[]>;
+}) => {
+  return <div>Filter goes here</div>;
+};
