c58cd1c2c03f2da488870b88f4102fd8f7d14fbb
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-16 15:57:50 +0200

    #76/feat: create an interface for movie-details

--- src/lib/interfaces/movie-interfaces.ts
+++ src/lib/interfaces/movie-interfaces.ts
@@ -1,3 +1,5 @@
+import { Genre } from "./category-interfaces";
+
 // Movieslist from API
 export interface MoviesResultsListFromApi {
   page: number;
@@ -24,27 +26,74 @@ export interface MovieObjectFromApi {
   vote_count: number;
 }
 
-// Movie object with updated properties
-// export interface Movie {
-//   id: number;
-//   adult: boolean;
-//   backdrop_path: string;
-//   genre_ids: number[];
-//   original_language: string;
-//   original_title: string;
-//   overview: string;
-//   popularity: number;
-//   poster_path: string;
-//   release_date: string;
-//   title: string;
-//   video: false;
-//   vote_average: number;
-//   vote_count: number;
-//   isFavourite: boolean; //new
-//   onWatchList: boolean; // new
-// }
-
 export interface Movie extends MovieObjectFromApi {
   isFavourite: boolean;
   isInWatchList: boolean;
+}
+
+export interface MovieDetailsFromApi {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: Collection;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: number;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompanies[];
+  production_countries: ProductionCountries[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_language: SpokenLanguages[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieDetails extends MovieDetailsFromApi {
+  isFavourite: boolean;
+  isInWatchList: boolean;
+}
+
+// Details
+export interface Collection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+export interface Country {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+export interface ProductionCompanies {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountries {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface SpokenLanguages {
+  english_name: string;
+  iso_696_1: string;
+  name: string;
 }
