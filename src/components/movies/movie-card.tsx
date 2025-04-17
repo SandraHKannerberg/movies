d5d211044e6405d8ca3d2058713dabb89304be5f
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-17 13:58:10 +0200

    #86/design: add hover effect to movie-card and update to h-full for same height to all cards

--- src/components/movies/movie-card.tsx
+++ src/components/movies/movie-card.tsx
@@ -2,20 +2,14 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import {
-  Card,
-  CardContent,
-  // CardFooter, //Save for later
-  // CardHeader, //Save for later
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Movie } from "@/lib/interfaces/movie-interfaces";
 
 export const MovieCard = ({ movie }: { movie: Movie }) => {
   return (
     <>
       <Link href={`/movies/${movie.id}`}>
-        <Card className="overflow-hidden rounded-lg shadow-md shadow-border border-1 border-border">
+        <Card className="h-full overflow-hidden rounded-lg shadow-md shadow-border border-1 border-border hover:shadow-2xl hover:shadow-primary transition-transform duration-300 hover:scale-105">
           <CardTitle className="sr-only">{movie.title}</CardTitle>
           <CardContent className="p-0">
             <Image
