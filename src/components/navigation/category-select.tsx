4fcdb97cd9ac3125faf239e1827344ef749be7f7
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-04-18 14:00:32 +0200

    #86/feat: show choosen category as value in select category, if no category - show 'all' as default.

--- src/components/navigation/category-select.tsx
+++ src/components/navigation/category-select.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { useQueryParams } from "../../../hooks/use-query-string";
 import {
@@ -18,6 +18,7 @@ export const CategorySelect = ({ categories }: { categories: Genre[] }) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const createQueryString = useQueryParams(searchParams);
+  const [selectedCategory, setSelectedCategory] = useState<string>("all");
 
   const allCategories = categories;
 
@@ -25,6 +26,28 @@ export const CategorySelect = ({ categories }: { categories: Genre[] }) => {
   if (!allCategories || allCategories.length <= 0) {
     throw new Error("No categories found");
   }
+
+  useEffect(() => {
+    const categoryFromSearch = searchParams.get("category");
+
+    if (categoryFromSearch) {
+      // Check if category from url exists in allCategories
+      const currentCategory = allCategories.find(
+        (cat) =>
+          cat.id ===
+          (categoryFromSearch === "all" ? 0 : Number(categoryFromSearch))
+      );
+
+      // If category exists --- set selected category
+      if (currentCategory) {
+        setSelectedCategory(currentCategory.id.toString());
+      } else {
+        setSelectedCategory("all");
+      }
+    } else {
+      setSelectedCategory("all");
+    }
+  }, [searchParams, allCategories]);
 
   const handleChange = (value: string): void => {
     const queryString = createQueryString({ category: value }, ["page"]);
@@ -36,7 +59,7 @@ export const CategorySelect = ({ categories }: { categories: Genre[] }) => {
       <Label htmlFor="category" className="sr-only">
         Select a category
       </Label>
-      <Select onValueChange={handleChange}>
+      <Select value={selectedCategory} onValueChange={handleChange}>
         <SelectTrigger name="category" id="category" className="cursor-pointer">
           <SelectValue placeholder="Select category" />
         </SelectTrigger>
